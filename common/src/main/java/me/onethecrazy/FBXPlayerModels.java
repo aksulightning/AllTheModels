5d369a67032e94f732ec195b7ac1a692b463b353
aksulightning <232076266+aksulightning@users.noreply.github.com>
2026-08-30 11:39:22 +0300

    Make nightly release

--- common/src/main/java/me/onethecrazy/FBXPlayerModels.java
+++ common/src/main/java/me/onethecrazy/FBXPlayerModels.java
@@ -1,7 +1,7 @@
 package me.onethecrazy;
 
 public final class FBXPlayerModels {
-    public static final String MOD_ID = "fbx-player-models";
+    public static final String MOD_ID = "fbx-player-models-lite";
     public static final String DISPLAY_NAME = "FBX Player Models Lite";
 
     private FBXPlayerModels() {
